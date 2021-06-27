bd2fa46d85489511b2fad128f6637d474407f6fd
Arran Dinsmore <arrandinsmore@protonmail.com>
2021-06-27 18:14:24 +0100

    added go-randdate.go

---
+++ go-randdate.go
@@ -0,0 +1,21 @@
+package randdate
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandDate(minYear, maxYear int) (day, date string) {
+	if minYear >= maxYear {
+		minYear = 1600
+		maxYear = 2200
+	}
+	min := time.Date(minYear, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
+	max := time.Date(minYear, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
+	delta := max - min
+	sec := rand.Int63n(delta) + min
+	fullDate := time.Unix(sec, 0)
+	day = fullDate.Format("Monday")
+	date = fullDate.Format("02-Jan-2006")
+	return day, date
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dustydins/go-randdate
+
+go 1.16
